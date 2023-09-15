b5d6e1a628fa468bb54e2eea452cf5dc72c66143
Daniel Fu <daniel.fu123@hotmail.com>
2023-09-15 14:31:43 -0400

    intial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+/*********************************************************************************
+*  BTI325 – Assignment 1
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: Daniel Fu    Student ID: 153024229   Date: Sept 15, 2023
+*
+*  Online (Cyclic) URL: _______________________________________________________
+*
+********************************************************************************/ 
+
+
+var HTTP_PORT = process.env.PORT || 8080;
+var express = require("express");
+var app = express();
+
+// setup a 'route' to listen on the default url path
+app.get("/", (req, res) => {
+    res.send("Daniel Fu - 153024229");
+});
+
+// setup http server to listen on HTTP_PORT
+app.listen(HTTP_PORT);
+
